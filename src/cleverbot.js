eb12a86668fe40187bbd78a5878a5f02abe90b35
code-hero-projects <code.hero.email@gmail.com>
2021-12-18 18:20:59 +0000

    cleverbot receiving and sending messages

--- src/cleverbot.js
+++ src/cleverbot.js
@@ -1,3 +1,49 @@
-const form = document.getElementById('noteb');
-const formButton = form.childNodes[1];
-formButton.submit();
+const understoodFormSubmitButton = document.getElementById('noteb').childNodes[1];
+understoodFormSubmitButton.submit();
+
+const messageForm = document.getElementById('avatarform');
+const input = messageForm.childNodes[0];
+const messageFormSubmitButton = messageForm.childNodes[6];
+
+const cleverbotResponseDiv = document.getElementById('line1');
+const currentResponseInput = cleverbotResponseDiv.childNodes[0];
+
+const getCleverbotResponse = onResponse => {
+  const answerInterval = setInterval(() => {
+    if (cleverbotResponseDiv.childNodes.length !== 1) {
+      clearInterval(answerInterval);
+      const response = currentResponseInput.textContent;
+      onResponse(response);
+    }
+  });
+};
+
+const respondToCleverbot = message => {
+  input.value = message;
+  messageFormSubmitButton.click();
+};
+
+const socket = new WebSocket('ws://localhost:8080');
+
+socket.addEventListener('open', event => {
+  const message = {
+    type: 'connection',
+    from: 'cleverbot'
+  };
+  socket.send(JSON.stringify(message));
+});
+
+socket.addEventListener('message', event => {
+  const message = event.data;
+  respondToCleverbot(message);
+
+  getCleverbotResponse(cleverbotResponse => {
+    const messageToSend = {
+      type: 'data',
+      to: 'omegle',
+      data: cleverbotResponse
+    };
+  
+    socket.send(JSON.stringify(messageToSend));
+  });
+});
