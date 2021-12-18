97c83d0f4830b2cd617b5f03f3464694fca8cdb2
code-hero-projects <code.hero.email@gmail.com>
2021-12-18 20:02:16 +0000

    have chat with cleverbot

--- src/omegle.js
+++ src/omegle.js
@@ -1,10 +1,29 @@
+const socket = new WebSocket('ws://localhost:8080');
 let currentMessagesLength = 0;
+
+socket.addEventListener('open', event => {
+  const message = {
+    type: 'connection',
+    from: 'omegle'
+  };
+  socket.send(JSON.stringify(message));
+});
+
+const sendSocketMessage = message => {
+  const messageToSend = {
+    type: 'data',
+    to: 'cleverbot',
+    data: message
+  };
+
+  socket.send(JSON.stringify(messageToSend));
+};
 
 const consentTermsAndConditions = () => {
   // TODO
 };
 
-const getNextMessage = (onMessage) => {
+const getNextMessage = onMessage => {
   const answerInterval = setInterval(() => {
     const strangerMessages = document.getElementsByClassName('strangermsg');
     const strangerMessagesLength = strangerMessages.length;
@@ -21,12 +40,25 @@ const getNextMessage = (onMessage) => {
   })
 };
 
+const replyToStranger = message => {
+  const textArea = document.getElementsByClassName('chatmsg ')[0];
+  const sendButton = document.getElementsByClassName('sendbtn')[0];
+  textArea.value = message;
+  sendButton.click();
+};
+
 const startChatting = () => {
   console.log('started');
   consentTermsAndConditions();
-  getNextMessage(message => {
-    console.log('message: '+ message);
+
+  socket.addEventListener('message', event => {
+    const message = event.data;
+    replyToStranger(message);
+
+    getNextMessage(sendSocketMessage);
   });
+
+  getNextMessage(sendSocketMessage);
 };
 
 document.getElementById('chattypetextcell').onclick = startChatting;
